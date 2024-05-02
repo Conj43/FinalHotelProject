bf48a197e7141283790d0be330aadb2d3416a62d
Drew Distler <dmd9g3@umsystem.edu>
2024-05-02 12:48:54 -0500

    made the command for checking in

--- FinalHotelProject/src/main/java/edu/mu/hotel/CheckInCommand.java
+++ FinalHotelProject/src/main/java/edu/mu/hotel/CheckInCommand.java
@@ -1,5 +1,21 @@
 package edu.mu.hotel;
 
-public class CheckInCommand {
+import edu.mu.customer.Customer;
 
+public class CheckInCommand implements CIOCommand {
+
+private CIOReciver cioReciver;
+	
+	public CheckInCommand(CIOReciver cioReciver) 
+	{
+
+		this.cioReciver = cioReciver;
+	}
+	
+	@Override
+	public void execute(Customer customer, Reservation reservation) {
+		cioReciver.checkin(customer, reservation);		
+	}
+
+	
 }
