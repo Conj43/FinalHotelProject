684483ece6544287a916b3abe39482e528a870c7
Drew Distler <dmd9g3@umsystem.edu>
2024-05-05 14:03:52 -0500

    made changes to check in check out logic and to add rewards points

--- FinalHotelProject/src/main/java/edu/mu/hotel/CIOReceiver.java
+++ FinalHotelProject/src/main/java/edu/mu/hotel/CIOReceiver.java
@@ -1,13 +1,19 @@
 package edu.mu.hotel;
 
 import edu.mu.customer.Customer;
+import edu.mu.hotel.rooms.RoomTypeManager;
 
 public class CIOReceiver {
+	
+	RoomTypeManager manager = RoomTypeManager.getInstance();
+	
 	public void checkin(Customer customer, Reservation reservation)
 	{
 		if(customer.getCustomerID() == reservation.getCustomerId())
 		{
-			reservation.setActive(true);	
+			reservation.setActive(true);
+			manager.decreaseRoomAvailability(reservation.getRoom());
+			customer.setRewardPoints(customer.getRewardsPoints()+40);
 		}
 	}
 	
@@ -16,6 +22,7 @@ public class CIOReceiver {
 		if(customer.getCustomerID() == reservation.getCustomerId())
 		{
 			reservation.setActive(false);	
+			manager.decreaseRoomAvailability(reservation.getRoom());
 		}
 	}
 }
