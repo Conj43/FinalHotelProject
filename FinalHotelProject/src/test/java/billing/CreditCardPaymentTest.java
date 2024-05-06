19c965d7716c725286d991b4c0ae5f5a1a74f91f
Drew Distler <dmd9g3@umsystem.edu>
2024-05-06 02:00:31 -0500

    made unit tests for Credit card strategy

---
+++ FinalHotelProject/src/test/java/billing/CreditCardPaymentTest.java
@@ -0,0 +1,26 @@
+package billing;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import edu.mu.customer.*;
+import edu.mu.billing.*;
+
+class CreditCardPaymentTest {
+
+    private PaymentStrategy paymentStrategy;
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        paymentStrategy = new CreditCardPayment();
+        customer = new Customer();
+    }
+
+    // Positive test for paying bill using credit card payment strategy
+    @Test
+    void testPayBill() {
+        double amount = 100.0;
+        assertDoesNotThrow(() -> paymentStrategy.pay(customer, amount));
+    }
+}
