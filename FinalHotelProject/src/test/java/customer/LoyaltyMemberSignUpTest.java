df96fe9b09d42227b8743b92965f4e7619f65749
Drew Distler <dmd9g3@umsystem.edu>
2024-05-06 02:01:09 -0500

    made unit tests for loyalty member sign up

---
+++ FinalHotelProject/src/test/java/customer/LoyaltyMemberSignUpTest.java
@@ -0,0 +1,18 @@
+package customer;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import edu.mu.customer.*;
+
+class LoyaltyMemberSignUpTest {
+	
+	//positive test for signing up for loyalty points
+
+	@Test
+	void testSignUp()
+	{
+		Customer customer = new Customer();
+		LoyaltyMemberSignUp.signUp(customer);
+		assertTrue(customer.isRewardsMember());
+		assertEquals(20, customer.getRewardsPoints());
+}
+}
